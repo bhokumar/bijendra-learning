a7ed100459807ad42393a98734c939abc0ca0244
rathorebijendra <rathorebijendra1999@gmail.com>
2020-05-03 13:36:30 +0530

    initial adding project

---
+++ react-complete-guide2/src/UserOutput/UserOutPut.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const UserOutput=(props)=>{
+    return(
+        <div>
+            <p>
+                some rendom text!
+              <p>  userName:{props.userName}</p>
+            </p>
+        </div>
+    );
+
+
+};
+export default UserOutput;
